a286df3f036791c354a1d1fa304f4be5b367abd3
郑力铖 <92080988+SHUzhekiNg@users.noreply.github.com>
2022-10-02 20:22:08 +0800

    0.1

--- 22Fall-OOP-Project1/express.h
+++ 22Fall-OOP-Project1/express.h
@@ -1,22 +1,20 @@
 #pragma once
 #include <iostream>
 #include <cstring>
+#include <vector>
 #include "LinkList.h"
 using namespace std;
 
-
 class Express {
 public:
-	Express(const string& id = "NULL", const string& name = "NONAME") :id(id), name(name) {}
+	Express(const int& id_things = 0, const string& name = "NONAME") :id_things(id_things), name(name) {}
 	friend ostream& operator<<(ostream& out, const Express& a);
 	friend istream& operator>>(istream& in, Express& a);
 
 
-
 private:
-	string id;
+	static vector<Express> alllist;			// 所有商品的名字和id的字典
+	int id_things;							// 种类编号
 	string name;
 };
 
-template <typename T>
-void Search(LinkList<LinkList<Express>>& link, const T& t);
