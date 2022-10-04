f8b005c6b3dcfd7fef6d36a4626f6a1ac050a44f
Scipio-00 <edenking@foxmail.com>
2022-10-04 20:12:02 +0800

    expressTest.h Updated

--- 22Fall-OOP-Project1/expressTest.cpp
+++ 22Fall-OOP-Project1/expressTest.cpp
@@ -2,13 +2,15 @@
 #include "LinkList.h"
 #include "express.h"
 #include <conio.h>
+#include <string>
 
 const int MAX_SIZE = 30;			//货物名称和ID的最大长度
 
 void expressTest()
 {
+	using std::cout;
 	LinkList<LinkList<Express>> WarehouseList;
-	Node<Express> *tmp;
+	Node<Express> *tmp=nullptr;
 	char id[80], name[80], str[80];
 	int choice;
 	int numW,numC;
@@ -27,17 +29,18 @@ void expressTest()
 		cout << endl;
 		cout << "1 -- 入库" << endl;
 		cout << "2 -- 出库" << endl;
-		cout << "3 -- 全局查询" << endl;
-		cout << "4 -- 逐仓查询" << endl;
+		cout << "3 -- 查询" << endl;
+		cout << "4 -- 盘点" << endl;
 		cout << "0 -- 返回" << endl;
 		cout << "请选择：";
 		choice = getche() - '0';
 		cout << endl;
 		if (choice <= 0) break;
 		Express Ex1;
+		string s;
 		switch (choice)
 		{
-		case 1:
+		case 1:											//入库
 			cout << "请输入入库仓库编号: ";
 			cin >> numW;
 			cin.getline(str, 80);					// 处理换行字符
@@ -45,7 +48,7 @@ void expressTest()
 			WarehouseList.Skip(numW - 1);
 			//入库
 			break;
-		case 2:	
+		case 2:											//出库
 			cout << "请输入出库仓库编号: ";
 			cin >> numW;
 			cin.getline(str, 80);					// 处理换行字符
@@ -53,24 +56,43 @@ void expressTest()
 			WarehouseList.Skip(numW - 1);
 			//出库
 			break;
-		case 3:
-			cout << "请输入所要查询货物的的ID: ";
-			cin >> numC;
-			cin.getline(str, 80);					// 处理换行字符
-			Ex1.SearchGlobal(WarehouseList,numC);
-			break;
-		case 4:
-			cout << "请输入所要查询货物的的ID: ";
-			cin >> numC;
-			cin.getline(str, 80);					// 处理换行字符
-			cout << "请输入所要查询的仓库: ";
+		case 3:											//查询
+			cout << "请输入所要查询的仓库: (输入0以查询所有仓库)";
 			cin >> numW;
+			cin.getline(str, 80);
+			cout << "请输入所要查询货物的的ID或名称:";
+			cin >> s;
 			cin.getline(str, 80);					// 处理换行字符
-			Express Ex1;
+			if (s[0] >= '0' && s[0] <= '9')
+			{
+				numC = stoi(s);
+				Ex1.SearchGlobal(WarehouseList, numC, numW);
+			}
+			else Ex1.SearchGlobal(WarehouseList, s, numW);
+			/*
 			WarehouseList.GoTop();
 			WarehouseList.Skip(numW - 1);
 			Ex1.SearchLocal((WarehouseList.CurNode())->Getdata(), numC);
 			break;
+			*/
+			break;
+		case 4:											//盘点
+			cout << "请输入所要盘点的仓库: (输入0以查询所有仓库)";
+			cin >> numW;
+			cin.getline(str, 80);
+			cout << "请输入所要查询货物的的ID或名称:";
+			cin >> s;
+			cin.getline(str, 80);					// 处理换行字符
+			WarehouseList.GoTop();
+			WarehouseList.Skip(numW - 1);
+			Ex1.SearchLocal((WarehouseList.CurNode())->Getdata(), numC);
+
+			if (s[0] >= '0' && s[0] <= '9')
+			{
+				numC = stoi(s);
+				Ex1.SearchLocal((WarehouseList.CurNode())->Getdata(), numC);
+			}
+			else Ex1.SearchLocal((WarehouseList.CurNode())->Getdata(), s);
 		}
 	}
 
